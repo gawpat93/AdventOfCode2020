147dcad5b58a07743ce1b11d8b4730be717f5aaa
Patryk Gawryszewski <patgaw09318@gmail.com>
2020-12-19 22:34:48 +0100

    Day1 - completed

---
+++ AdventOfCode2020/Day1/Tests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace Day1
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void SimpleTest()
+        {
+            var result1 = Tools.MultiplyTwoElementsOfSearchedSum(@"Data\testData0.dat");
+            Assert.AreEqual(514579, result1);
+        }
+
+        [Test]
+        public void FinalTest()
+        {
+            var result2 = Tools.MultiplyTwoElementsOfSearchedSum(@"Data\input.dat");
+            Assert.AreEqual(55776, result2);
+        }
+    }
+}
---
+++ AdventOfCode2020/Day1/Tools.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day1
+{
+    public static class Tools
+    {
+        private const int SEARCHED_SUM = 2020;
+
+        public static int MultiplyTwoElementsOfSearchedSum(string inputFileName)
+        {
+            var lines = File.ReadAllLines(inputFileName);
+            var array = lines.Select(int.Parse).ToArray();
+
+            for (var i = 0; i < array.Length; i++)
+            {
+                for (var j = 0; j < array.Length; j++)
+                {
+                    if (i == j) continue;
+
+                    if (array[i] + array[j] == SEARCHED_SUM)
+                    {
+                        return array[i] * array[j];
+                    }
+                }
+            }
+
+            throw new Exception("Not found");
+        }
+    }
+}
