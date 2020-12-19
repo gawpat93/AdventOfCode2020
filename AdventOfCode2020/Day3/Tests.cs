a5aca73336d7c0827b06eb65c0d7bd5eb224e784
Patryk Gawryszewski <patgaw09318@gmail.com>
2020-12-20 00:36:32 +0100

    Day3 - project created

---
+++ AdventOfCode2020/Day3/Tests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace Day3
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void FinalTest()
+        {
+            //var result1 = Tools.ValidatePasswords(@"Data\input.dat");
+            //Assert.AreEqual(564, result1);
+            
+            //var result2 = Tools.ValidatePasswordsWithNewPolicy(@"Data\input.dat");
+            //Assert.AreEqual(325, result2);
+        }
+
+        [Test]
+        public void SimpleTest()
+        {
+            //var result1 = Tools.ValidatePasswords(@"Data\testData0.dat");
+            //Assert.AreEqual(2, result1);
+            
+            //var result2 = Tools.ValidatePasswordsWithNewPolicy(@"Data\testData0.dat");
+            //Assert.AreEqual(1, result2);
+        }
+    }
+}
