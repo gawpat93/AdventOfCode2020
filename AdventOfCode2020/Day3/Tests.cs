3a7fd0a02119c43ef8902ff8aca4ebac8e5bb0ca
Patryk Gawryszewski <patgaw09318@gmail.com>
2020-12-20 01:09:44 +0100

    Day3 - completed

--- AdventOfCode2020/Day3/Tests.cs
+++ AdventOfCode2020/Day3/Tests.cs
@@ -8,21 +8,30 @@ namespace Day3
         [Test]
         public void FinalTest()
         {
-            //var result1 = Tools.ValidatePasswords(@"Data\input.dat");
-            //Assert.AreEqual(564, result1);
-            
-            //var result2 = Tools.ValidatePasswordsWithNewPolicy(@"Data\input.dat");
-            //Assert.AreEqual(325, result2);
+            var result0 = Tools.CountEncounteredTrees(@"Data\input.dat",1,1);
+            var result1 = Tools.CountEncounteredTrees(@"Data\input.dat");
+            Assert.AreEqual(171, result1);
+            var result2 = Tools.CountEncounteredTrees(@"Data\input.dat", 5, 1);
+            var result3 = Tools.CountEncounteredTrees(@"Data\input.dat", 7, 1);
+            var result4 = Tools.CountEncounteredTrees(@"Data\input.dat", 1, 2);
+            Assert.AreEqual(1206576000, result0 * result1 * result2 * result3 * result4);
         }
 
         [Test]
         public void SimpleTest()
         {
-            //var result1 = Tools.ValidatePasswords(@"Data\testData0.dat");
-            //Assert.AreEqual(2, result1);
-            
-            //var result2 = Tools.ValidatePasswordsWithNewPolicy(@"Data\testData0.dat");
-            //Assert.AreEqual(1, result2);
+            var result0 = Tools.CountEncounteredTrees(@"Data\testData0.dat", 1, 1);
+            Assert.AreEqual(2, result0);
+            var result1 = Tools.CountEncounteredTrees(@"Data\testData0.dat");
+            Assert.AreEqual(7, result1);
+            var result2 = Tools.CountEncounteredTrees(@"Data\testData0.dat", 5, 1);
+            Assert.AreEqual(3, result2);
+            var result3 = Tools.CountEncounteredTrees(@"Data\testData0.dat", 7, 1);
+            Assert.AreEqual(4, result3);
+            var result4 = Tools.CountEncounteredTrees(@"Data\testData0.dat", 1, 2);
+            Assert.AreEqual(2, result4);
+            Assert.AreEqual(336, result0 * result1 * result2 * result3 * result4);
+
         }
     }
 }
--- AdventOfCode2020/Day3/Tools.cs
+++ AdventOfCode2020/Day3/Tools.cs
@@ -1,7 +1,26 @@
-﻿namespace Day3
+﻿using System.ComponentModel;
+using System.IO;
+
+namespace Day3
 {
     public static class Tools
     {
-        
+        private const char TREE = '#';
+        public static int CountEncounteredTrees(string inputFileName,int slopeX=3,int slopeY = 1)
+        {
+            var lines = File.ReadAllLines(inputFileName);
+            var length = lines[0].Length; 
+            var treeCount = 0;
+            var x = 0;
+            for (var i = 0; i<lines.Length;i+=slopeY)
+            {
+                var current = lines[i];
+                if (current[x] == TREE) treeCount++;
+                x += slopeX;
+                if (x >= length) x -= length;
+            }
+
+            return treeCount;
+        }
     }
 }
