a686aca89d589462d40a9a27703369a0f1de3bd3
Patryk Gawryszewski <patgaw09318@gmail.com>
2020-12-21 22:44:04 +0100

    Day7 - project added

---
+++ AdventOfCode2020/Day7/Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+namespace Day7
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void FinalTest()
+        {
+        }
+
+        [Test]
+        public void SimpleTest()
+        {
+            var result = Tools.GetNumberOfBagsAbleToContainMyBag(@"Data\simple.dat");
+            Assert.AreEqual(4, result);
+        }
+    }
+}
---
+++ AdventOfCode2020/Day7/Tools.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace Day7
+{
+    public static class Tools
+    {
+        private const string MY_BAG_COLOR = "shiny gold";
+        
+        public static int GetNumberOfBagsAbleToContainMyBag(string inputFileName)
+        {
+            var lines = File.ReadAllLines(inputFileName);
+
+
+            return 0;
+        }
+    }
+}
