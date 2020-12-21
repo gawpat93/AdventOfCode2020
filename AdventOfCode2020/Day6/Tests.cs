ee9921a788a1b97a0d58c808e1006d26e843d274
Patryk Gawryszewski <patgaw09318@gmail.com>
2020-12-21 21:17:19 +0100

    Day6 - part1 completed

---
+++ AdventOfCode2020/Day6/Tests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+namespace Day6
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void FinalTest()
+        {
+            var result = Tools.QuestionsAnsweredYes(@"Data\input.dat");
+            Assert.AreEqual(6443, result);
+        }
+
+        [Test]
+        public void SimpleTest()
+        {
+            var result = Tools.QuestionsAnsweredYes(@"Data\simple.dat");
+            Assert.AreEqual(11, result);
+        }
+    }
+}
---
+++ AdventOfCode2020/Day6/Tools.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Day6
+{
+    public static class Tools
+    {
+        public static int QuestionsAnsweredYes(string inputFileName)
+        {
+            var lines = File.ReadAllLines(inputFileName);
+            var groups = new List<string>();
+            var current = new StringBuilder();
+            for (var i = 0; i < lines.Length; i++)
+            {
+                if (string.IsNullOrWhiteSpace(lines[i]))
+                {
+                    groups.Add(current.ToString());
+                    current.Clear();
+                }
+                else
+                {
+                    current.Append($"{lines[i]}");
+                }
+            }
+
+            groups.Add(current.ToString());
+            current.Clear();
+
+            var sum = 0;
+            groups.ForEach(group => sum += group.ToCharArray().ToList().Distinct().Count());
+            return sum;
+        }
+    }
+}
