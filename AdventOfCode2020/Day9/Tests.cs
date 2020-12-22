d1741e753f8a5da0b7ab3572f1bd2c6680c31e19
Patryk Gawryszewski <patgaw09318@gmail.com>
2020-12-22 22:16:49 +0100

    Day9 - part 1 completed

---
+++ AdventOfCode2020/Day9/Tests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+namespace Day9
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void FinalTest()
+        {
+            var result = Tools.GetNotSumNumberAfterPreamble(@"Data\input.dat", 25);
+            Assert.AreEqual(375054920, result);
+        }
+
+        [Test]
+        public void SimpleTest()
+        {
+            var result = Tools.GetNotSumNumberAfterPreamble(@"Data\simple.dat", 5);
+            Assert.AreEqual(127, result);
+        }
+    }
+}
---
+++ AdventOfCode2020/Day9/Tools.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using static System.Int64;
+
+namespace Day9
+{
+    public static class Tools
+    {
+
+        public static long GetNotSumNumberAfterPreamble(string inputFileName, int preambleSize)
+        {
+            var lines = File.ReadAllLines(inputFileName);
+            var values = lines.Select(Parse).ToArray();
+            for (var index = preambleSize; index < values.Length; index++)
+            {
+                bool isSumOfPreviousTwoValues = false;
+                var current = values[index];
+                var previousValues = values[(index - preambleSize)..index];
+                for (var i = 0; i < previousValues.Length; i++)
+                {
+                    if (isSumOfPreviousTwoValues)
+                    {
+                        break;
+                    }
+
+                    for (var j = 0; j < previousValues.Length; j++)
+                    {
+                        if (i == j) continue;
+
+                        if (previousValues[i] + previousValues[j] == current)
+                        {
+                            isSumOfPreviousTwoValues = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (!isSumOfPreviousTwoValues)
+                {
+                    return current;
+                }
+            }
+
+            throw new Exception("Error in Tools.GetNotSumNumberAfterPreamble");
+        }
+    }
+}
