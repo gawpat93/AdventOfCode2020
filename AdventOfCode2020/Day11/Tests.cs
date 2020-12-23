3f9ed75ea74525a21b8775c995c93d29699d2bed
Patryk Gawryszewski <patgaw09318@gmail.com>
2020-12-23 23:40:00 +0100

    Day11 - project added

---
+++ AdventOfCode2020/Day11/Tests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace Day11
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void FinalTest()
+        {
+
+        }
+
+        [Test]
+        public void SimpleTest()
+        {
+            var result = Tools.GetResultPart1(@"Data\simple.dat");
+            Assert.AreEqual(37, result);
+        }
+    }
+}
---
+++ AdventOfCode2020/Day11/Tools.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace Day11
+{
+    public static class Tools
+    {
+        public static long GetResultPart1(string inputFileName)
+        {
+            var lines = File.ReadAllLines(inputFileName);
+            return 0;
+        }
+    }
+}
