fc1e00b8c06d1be113cd973069a4bf8ba579c63a
Patryk Gawryszewski <patgaw09318@gmail.com>
2020-12-23 19:12:35 +0100

    Day10 - part1 completed

---
+++ AdventOfCode2020/Day10/Tests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace Day10
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void FinalTest()
+        {
+            var result = Tools.GetOneJoltMultipliedByThreeJoltDifferences(@"Data\input.dat");
+            Assert.AreEqual(1917, result);
+        }
+
+        [Test]
+        public void SimpleTest()
+        {
+            var result = Tools.GetOneJoltMultipliedByThreeJoltDifferences(@"Data\simple.dat");
+            Assert.AreEqual(5 * 7, result);
+
+            var result1 = Tools.GetOneJoltMultipliedByThreeJoltDifferences(@"Data\simple1.dat");
+            Assert.AreEqual(22 * 10, result1);
+        }
+    }
+}
---
+++ AdventOfCode2020/Day10/Tools.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day10
+{
+    public static class Tools
+    {
+
+        public static int GetOneJoltMultipliedByThreeJoltDifferences(string inputFileName)
+        {
+            var lines = File.ReadAllLines(inputFileName);
+            var jolts = lines.Select(int.Parse).OrderBy(x => x).ToArray();
+            var gaps = new List<int> { jolts[0] };
+            for (var i = 0; i < jolts.Length - 1; i++)
+            {
+                gaps.Add(jolts[i + 1] - jolts[i]);
+            }
+            gaps.Add(3); // difference between las jolt and my device
+
+            return gaps.Count(x => x == 3) * gaps.Count(x => x == 1);
+        }
+    }
+}
