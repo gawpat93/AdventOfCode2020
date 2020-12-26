adcf6f17d6ef29cd631b6ea0210c3f2c363303ef
Patryk Gawryszewski <patgaw09318@gmail.com>
2020-12-26 20:15:54 +0100

    Day13 - project added

---
+++ AdventOfCode2020/Day13/Tests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace Day13
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void FinalTest()
+        {
+            var result = Tools.GetResultPart1(@"Data\input.dat");
+            Assert.AreEqual(0, result);
+
+            var result2 = Tools.GetResultPart2(@"Data\input.dat");
+            Assert.AreEqual(0, result2);
+        }
+
+        [Test]
+        public void SimpleTest()
+        {
+            var result = Tools.GetResultPart1(@"Data\simple.dat");
+            Assert.AreEqual(295, result);
+
+            var result2 = Tools.GetResultPart2(@"Data\simple.dat");
+            Assert.AreEqual(0, result2);
+        }
+    }
+}
---
+++ AdventOfCode2020/Day13/Tools.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Day13
+{
+    public static class Tools
+    {
+
+        public static long GetResultPart1(string inputFileName)
+        {
+            var lines = File.ReadAllLines(inputFileName);
+            throw new NotImplementedException();
+        }
+
+        public static long GetResultPart2(string inputFileName)
+        {
+            var lines = File.ReadAllLines(inputFileName);
+            throw new NotImplementedException();
+        }
+    }
+}
