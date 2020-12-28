49259246ee1faae6eb24161879ac25098e75135e
Patryk Gawryszewski <patgaw09318@gmail.com>
2020-12-28 02:03:38 +0100

    Day14 - project created

---
+++ AdventOfCode2020/Day14/Tests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Day14
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void FinalTest()
+        {
+            //Assert.AreEqual(0, Tools.GetResultPart1(@"Data\input.dat"));
+        }
+
+        [Test]
+        public void SimpleTest()
+        {
+            Assert.AreEqual(165, Tools.GetResultPart1(@"Data\simple.dat"));
+        }
+    }
+}
---
+++ AdventOfCode2020/Day14/Tools.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Day14
+{
+    public static class Tools
+    {
+        public static long GetResultPart1(string inputFileName)
+        {
+            var lines = File.ReadAllLines(inputFileName);
+            throw new NotImplementedException();
+        }
+
+        public static long GetResultPart2(string inputFileName)
+        {
+            var lines = File.ReadAllLines(inputFileName);
+            throw new NotImplementedException();
+        }
+    }
+}
